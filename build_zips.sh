ca5c81c0c54ba80557d11f2cda985d1749abc730
Subin Siby <mail@subinsb.com>
2021-09-12 02:01:53 +0530

    Separate import script, add readme to be included

--- build_zips.sh
+++ build_zips.sh
@@ -7,5 +7,11 @@ for schemeID in ${schemes[@]}; do
   cp ../install.sh.in $schemeID/install.sh
   sed -i "s#@INSTALL_PREFIX@#/usr/local#g" $schemeID/install.sh
   chmod +x $schemeID/install.sh
+
+  cp ../import.sh.in $schemeID/import.sh
+  chmod +x $schemeID/import.sh
+
+  cp ../README.txt.in $schemeID/README.md
+
   zip -r ../$schemeID.zip $schemeID -x '*.scheme' -x '*.txt' -x '*.vst.learnings*'
 done
